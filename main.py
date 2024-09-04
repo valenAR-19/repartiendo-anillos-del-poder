7b803629db2abdb77f5e0b69d435dc6869f6e787
valenAR-19 <valentinbarberis007@gmail.com>
2024-09-04 17:49:48 -0300

    Continuando con el proyecto.

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+def is_prime(number: int):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if(number % i) == 0:
+            return False
+    return True
+
 def distribute_rings(total_rings: int):
     
     sauron = 1
     total_rings -= sauron
+    distributed_rings = []
     
     for men in range(2, total_rings, 2):
         for elves in range(1, total_rings, 2):
             dwarves = total_rings - men - elves
-            
-            return {
-                "Hombres": men,
-                "Elfos": elves,
-                "Enanos": dwarves,
-                "Sauron": sauron
-            }
+            if dwarves > 0 and is_prime(dwarves):
+                distributed_rings.append({
+                    "Hombres": men,
+                    "Elfos": elves,
+                    "Enanos": dwarves,
+                    "Sauron": sauron
+                })
+    
+    if distributed_rings:
+        return distributed_rings
     
     return "No es posible distribuir los anillos de poder."
 
@@ -21,5 +33,14 @@ try:
         input("Introduce el numero de anillos que quieres repartir: ")
     )
     distributed_rings = distribute_rings(total_rings)
+    
+    if isinstance(distributed_rings, list):
+        print("Posibles distribuciones de los anillos de poder: \n")
+        
+        for index, distribution in enumerate(distributed_rings):
+            print(f"{index + 1}. {distribution}")
+        
+    else:
+        print(distributed_rings)
 except ValueError:
     print("Por favor, introduce un numero entero de anillos.")
