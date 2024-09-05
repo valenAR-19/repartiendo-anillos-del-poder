84f5ed6824d31afc043f78ce1c5297d8afdb961b
valenAR-19 <valentinbarberis007@gmail.com>
2024-09-05 12:59:02 -0300

    Finalizando proyecto.

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ try:
         for index, distribution in enumerate(distributed_rings):
             print(f"{index + 1}. {distribution}")
         
+        print()
+        
+        print(f"Distribucion media {
+            distributed_rings[int(len(distributed_rings) / 2)]}"
+        )
     else:
         print(distributed_rings)
 except ValueError:
